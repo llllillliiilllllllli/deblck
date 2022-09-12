a5c23ad2273619c08ce610a8c40e63cc9269a988
dusthink <nguyenquocdung4869@gmail.com>
2022-09-12 15:01:39 +0700

    Build project structure and antifacts

---
+++ src/Application/Common/Execute.py
@@ -0,0 +1,12 @@
+from typing import Any, Tuple
+from Utils import Log
+
+def execute(selection: Tuple[Any, Any]) -> None:
+    Log.info(f"Execute command: {selection[0].__name__}.{selection[1].__name__}")
+
+    try: 
+        selection[1]()
+    except Exception as ex:
+        Log.error(ex)
+
+    return None
---
+++ src/Application/Main.py
@@ -0,0 +1,35 @@
+from datetime import datetime  
+import traceback
+
+from Config import *
+from Styles import UI
+from Common import Inspect  
+from Common import Select  
+from Common import Execute  
+
+def main():
+
+    UI.print_console() 
+
+    try: 
+        features = Inspect.inspect_features()
+        selection = Select.inquire_selection(features)
+        execution = Execute.execute(selection) 
+
+    except Exception:
+        traceback.print_exc()
+
+    selection = input("\nContinue (Y/N): ")
+    if selection == "Y":
+        main()
+    else:
+        return
+
+if __name__ == '__main__':
+    beg = datetime.now()
+    main()
+    end = datetime.now()
+    duration = end - beg
+    duration = duration.total_seconds()
+    
+    print(f"\nProgram finished in {duration:.2f} sec")
