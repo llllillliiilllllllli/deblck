8a57eafcd7a4b4f7315149e3d2be50036d141cc5
dusthink <nguyenquocdung4869@gmail.com>
2022-09-13 23:05:54 +0700

    Fix some spelling errors

--- src/Application/Operation/Execute.py
+++ src/Application/Operation/Execute.py
@@ -1,12 +1,17 @@
 from typing import Any, Tuple
 from Utils import Log
+import traceback
 
 def execute(selection: Tuple[Any, Any]) -> None:
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     Log.info(f"Execute command: {selection[0].__name__}.{selection[1].__name__}")
 
     try: 
         selection[1]()
     except Exception as ex:
         Log.error(ex)
+        traceback.print_exc()
 
+    Log.info(f"Finish command: {selection[0].__name__}.{selection[1].__name__}")
     return None
